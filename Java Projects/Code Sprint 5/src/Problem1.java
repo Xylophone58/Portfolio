3f2f91f450e05d1f7953436dd351ceff1132041e
Xylophone58 <89533905+Xylophone58@users.noreply.github.com>
2024-02-09 11:31:44 -0600

    Delete Java Projects directory

--- Java Projects/Code Sprint 5/src/Problem1.java
+++
@@ -1,7 +0,0 @@
-public class Problem1 {
-    public static void main(String[] args) {
-        for(int i = 3; i <= 30; i+= 5){
-            System.out.print(i + " ");
-        }
-    }
-}
--- Java Projects/Code Sprint/src/CodeSprint.java
+++
@@ -1,9 +0,0 @@
-public class CodeSprint {
-    public static void main(String[] args) {
-        System.out.println("");
-        System.out.println("3");
-        System.out.println("2");
-        System.out.println("1");
-        System.out.println("Blast Off!");
-    }
-}
